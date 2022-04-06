ff3060a7fa2d5ae2db138a78d21a4c5e2952d02c
kovvurichanakyareddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-04-06 20:21:56 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int n,i,even=0,odd=0;     scanf("%d",&n);     int *arr;     arr=(int *)malloc(n*sizeof(int));     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             even=even+1;         }else{             odd=odd+1;         }     }     printf("%d %d",even,odd);     return 0; }

---
+++ Count.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,even=0,odd=0;
+    scanf("%d",&n);
+    int *arr;
+    arr=(int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            even=even+1;
+        }else{
+            odd=odd+1;
+        }
+    }
+    printf("%d %d",even,odd);
+    return 0;
+}

f4c2e98859153094571d5ec102ba30803f25e827
kovvurichanakyareddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-04-13 19:48:40 +0530

    #include<stdio.h> int main() {     int n,m,mst=0;     scanf("%d",&n);     while(n>0)     {         m=n%10;         if(m>mst)         {             mst=m;         }         n/=10;     }     printf("%d",mst);     return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,mst=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        m=n%10;
+        if(m>mst)
+        {
+            mst=m;
+        }
+        n/=10;
+    }
+    printf("%d",mst);
+    return 0;
+}

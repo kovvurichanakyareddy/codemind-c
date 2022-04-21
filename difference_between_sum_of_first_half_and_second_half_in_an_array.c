d3669096ea374ca82c9457bb2b1fa0a10cf775b6
kovvurichanakyareddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-04-21 20:13:01 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int n,*arr,i,sum1=0,sum2=0;     scanf("%d",&n);     arr=(int *)malloc(n*sizeof(int));     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n/2;i++)     {         sum1+=arr[i];     }     for(i=n/2;i<n;i++)     {         sum2+=arr[i];     }     printf("%d",sum2-sum1);     return 0; }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,*arr,i,sum1=0,sum2=0;
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        sum1+=arr[i];
+    }
+    for(i=n/2;i<n;i++)
+    {
+        sum2+=arr[i];
+    }
+    printf("%d",sum2-sum1);
+    return 0;
+}

f0c1ab3f6d4b93dfe5ec34e53876c16d86f034b9
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-05-21 11:17:19 +0530

    #include<stdio.h> int main() {     int n,arr[200],i,j,s,k=0,t=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 s++;             }         }         if(s==1)         {             if(arr[i]>t)             {             t=arr[i];             }             k=1;         }     }     if(k==0)     {         printf("-1");     }else{         printf("%d",t);     } }

---
+++ UNIQUE_MAXIMUM_NUMBER.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[200],i,j,s,k=0,t=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                s++;
+            }
+        }
+        if(s==1)
+        {
+            if(arr[i]>t)
+            {
+            t=arr[i];
+            }
+            k=1;
+        }
+    }
+    if(k==0)
+    {
+        printf("-1");
+    }else{
+        printf("%d",t);
+    }
+}
